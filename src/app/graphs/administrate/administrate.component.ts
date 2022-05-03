5682b0297a5ae9bc38fe7fd44f971f035f5b031f
LEONARDO SANCHEZ <leo.sanchez.dxd@gmail.com>
2022-05-03 08:20:41 -0500

    Creacios visual de los grafos

--- src/app/graphs/administrate/administrate.component.ts
+++ src/app/graphs/administrate/administrate.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { Link } from 'src/app/models/link';
 import { Node } from 'src/app/models/node';
+import { fromEvent, Subscription } from 'rxjs';
 import * as d3 from 'd3';
 
 @Component({
@@ -13,58 +14,68 @@ export class AdministrateComponent implements OnInit, OnDestroy {
   nodes: Node[] = [];
   links: Link[] = [];
   simulation?: d3.Simulation<Node, undefined>;
+  link?: d3.Selection<SVGLineElement, Link, any, any>;
+  node?: d3.Selection<SVGCircleElement, Node, any, any>;
+  svg?: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>;
+  subscriptor?: Subscription;
 
-  constructor() {
+  constructor(private ngZone: NgZone) {
     this.graph = JSON.parse(localStorage.getItem('Graph')!);
   }
 
   ngOnInit(): void {
     this.nodes = this.graph.nodes;
-    this.simulation = this.createSimulation();
-
-    var svg = this.createSVG();
-
-    this.createNodes(svg);
-
     this.initLinks();
+    this.simulation = this.createSimulation();
+    this.svg = this.createSVG();
+    this.link = this.createLinks();
+    this.node = this.createNodes();
+    const obs = fromEvent(document.getElementsByClassName('graph')!, 'click');
+    this.subscriptor = obs.subscribe((evt) => {
+      typeof this.link !== 'undefined'
+        ? this.link!.attr('x1', (d: Link) => d.Source.x!)
+            .attr('x2', (d: Link) => d.Target.x!)
+            .attr('y1', (d: Link) => d.Source.y!)
+            .attr('y2', (d: Link) => d.Target.y!)
+        : console.log('alv todo');
 
-    this.createLinks(svg);
+      typeof this.node !== 'undefined'
+        ? this.node!.attr('cx', (d: Node) => d['x']!).attr('cy', (d) => d['y']!)
+        : console.log('X2');
+    });
   }
 
   initLinks(): void {
-    var source;
-    var target;
+    var source: Node;
+    var target: Node;
     const feo = new Node(1, '', {}, {}, 1, 1);
-    var index;
-    var sources: Array<Node> = [];
-    var targets = [];
-    this.graph.edges.forEach((element: any, index: number) => {
-      sources.push(this.nodes.find((item) => (item.index = element.source))!);
+    this.graph.edges.forEach((element: any) => {
+      this.nodes.forEach((item: Node) => {
+        if (element.source == item.index) {
+          source = item;
+        }
+        if (element.target == item.index) {
+          target = item;
+        }
+      });
+      this.links.push(
+        new Link(
+          element.index!,
+          typeof source !== 'undefined' ? source : feo,
+          typeof target !== 'undefined' ? target : feo,
+          element.weight!
+        )
+      );
     });
-    console.log(sources);
-    this.graph.edges.forEach((element: any, index: number) => {
-      targets.push(this.nodes.find((item) => (item.index = element.target)));
-    });
-
-    /* this.links.push(
-      new Link(
-        index,
-        typeof source === 'object' ? source : feo,
-        typeof target === 'object' ? target : feo,
-        100
-      )
-    ); */
-    this.links.push(new Link(1, this.nodes[1], this.nodes[0], 1000));
-    //console.log(this.links);
-    this.simulation?.force('link', d3.forceLink(this.links));
   }
 
   createSimulation(): d3.Simulation<Node, undefined> {
     return d3
       .forceSimulation(this.nodes)
+      .force('link', d3.forceLink(this.links))
       .force('charge', d3.forceManyBody())
       .force('center', d3.forceCenter())
-      .force('collide', d3.forceCollide(25));
+      .force('collide', d3.forceCollide(40));
   }
 
   createSVG() {
@@ -76,21 +87,18 @@ export class AdministrateComponent implements OnInit, OnDestroy {
       .attr('viewBox', [-200, -300, 400, 600]);
   }
 
-  createNodes(svg: d3.Selection<any, any, any, any>) {
-    const nodes = svg
-      .selectAll('circle')
+  createNodes(): d3.Selection<SVGCircleElement, Node, any, any> {
+    return this.svg!.selectAll('circle')
       .data(this.nodes)
       .enter()
       .append('circle')
-      .attr('cx', (d) => d['x']!)
-      .attr('cy', (d) => d['y']!)
-      .attr('r', 5)
-      .on('click', this.dragged);
+      .attr('cx', (d: Node) => d['x']!)
+      .attr('cy', (d: Node) => d['y']!)
+      .attr('r', 10);
   }
 
-  createLinks(svg: d3.Selection<any, any, any, any>) {
-    const links = svg
-      .selectAll('line')
+  createLinks(): d3.Selection<SVGLineElement, Link, any, any> {
+    return this.svg!.selectAll('line')
       .style('stroke', 'red')
       .style('stroke-width', 2)
       .data(this.links)
@@ -98,18 +106,27 @@ export class AdministrateComponent implements OnInit, OnDestroy {
       .append('line')
       .style('stroke', 'red')
       .style('stroke-width', 2)
-      .attr('x1', (d) => d.Source.x!)
-      .attr('x2', (d) => d.Target.x!)
-      .attr('y1', (d) => d.Source.y!)
-      .attr('y2', (d) => d.Target.y!);
+      .attr('x1', (d: Link) => d.Source.x!)
+      .attr('x2', (d: Link) => d.Target.x!)
+      .attr('y1', (d: Link) => d.Source.y!)
+      .attr('y2', (d: Link) => d.Target.y!);
   }
 
   ngOnDestroy(): void {
     localStorage.removeItem('Graph');
+    this.subscriptor?.unsubscribe();
   }
 
-  dragged(event: any, d: Node) {
-    console.log('funciona' + d.index);
-    //this.nodes.push(new Node()
+  ticket() {
+    typeof this.link !== 'undefined'
+      ? this.link!.attr('x1', (d: Link) => d.Source.x!)
+          .attr('x2', (d: Link) => d.Target.x!)
+          .attr('y1', (d: Link) => d.Source.y!)
+          .attr('y2', (d: Link) => d.Target.y!)
+      : console.log('alv todo');
+
+    typeof this.node !== 'undefined'
+      ? this.node!.attr('cx', (d: Node) => d['x']!).attr('cy', (d) => d['y']!)
+      : console.log('X2');
   }
 }
